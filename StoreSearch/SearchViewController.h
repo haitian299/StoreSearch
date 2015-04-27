27af06797581678dc4668bf5feede1da77ca652e
金海天 <haitian299@gmail.com>
2015-04-27 22:42:25 +0800

    delete storyboard and create xib

--- StoreSearch/SearchViewController.h
+++ StoreSearch/SearchViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SearchViewController.h
 //  StoreSearch
 //
 //  Created by JIN on 15/4/27.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SearchViewController : UIViewController
 
 @end
-
