6fcce7726e7db018010e5de942046faf8d2cd3a4
金海天 <haitian299@gmail.com>
2015-04-27 19:33:26 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by JIN on 15/4/27.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by JIN on 15/4/27.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
