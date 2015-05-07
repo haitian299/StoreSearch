5afcafe1ad00965d2a100f1fcb37efe443e9d00d
金海天 <haitian299@gmail.com>
2015-05-07 22:21:40 +0800

    page 119

---
+++ StoreSearch/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  StoreSearch
+//
+//  Created by JIN on 15/5/7.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
