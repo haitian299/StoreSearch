c1567eaaaee5312b45d66f4b4cc4b6f73df28853
金海天 <haitian299@gmail.com>
2015-05-10 22:49:24 +0800

    page 149

---
+++ StoreSearch/GradientView.h
@@ -0,0 +1,13 @@
+//
+//  GradientView.h
+//  StoreSearch
+//
+//  Created by JIN on 15/5/10.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GradientView : UIView
+
+@end
